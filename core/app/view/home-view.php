e83cdcc4b6d61bb79edd64c5c6ee4b6245d075a8
JuanCarlos613 <61531129+JuanCarlos613@users.noreply.github.com>
2020-10-09 20:14:51 -0600

    Add files via upload

---
+++ core/app/view/home-view.php
@@ -0,0 +1,6 @@
+<div class="row">
+	<div class="col-md-12">
+		<h1 class="text-center"><strong> BIENVENIDO A OMNICORP</strong></h1>
+		<h1 class="text-center"><strong> TU MEJOR OPCION EN TECNOLOGIA</strong></h1><br>
+</div>
+
